e754a638f4ba3e4e50c23f429361a0b6cbae14a6
Douglas Barbosa <Doug_b_souza@hotmail.com>
2025-10-25 13:35:28 -0300

    Implement grayscale and binarization functions
    Added functions to convert an image to grayscale and binarize it.

---
+++ dimensionality-reduction.py
@@ -0,0 +1,34 @@
+from PIL import Image
+
+def to_grayscale(image_path):
+    image = Image.open(image_path).convert("RGB")
+    gray = Image.new("L", image.size)
+    w, h = image.size
+    
+    for x in range(w):
+        for y in range(h):
+            r, g, b = image.getpixel((x, y))
+            gray_val = int(0.299 * r + 0.587 * g + 0.114 * b)
+            gray.putpixel((x, y), gray_val)
+    
+    return gray
+
+def binarize_image(gray_image, threshold=128):
+
+    bw = Image.new("1", gray_image.size) 
+    w, h = gray_image.size
+    
+    for x in range(w):
+        for y in range(h):
+            gray_val = gray_image.getpixel((x, y))
+            bw.putpixel((x, y), 255 if gray_val > threshold else 0)
+    
+    return bw
+
+image_path = 'programacao/data-science/Estudo/Formacao_dio_machine_learning/Projetos_formacao/Redução de Dimensionalidade em Imagens para Redes Neurais/imagens/Serial Experiments Lain Wallpaper HD Pictures For.jpg'
+
+gray_img = to_grayscale(image_path)
+gray_img.show() 
+
+bw_img = binarize_image(gray_img, threshold=128)
+bw_img.show()    
